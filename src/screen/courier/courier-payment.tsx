1bddefa43e1f6d8cedbc5762e72c736cd1c3d5e2
alexchtst-01 <acinatra@gmail.com>
2026-01-18 16:29:24 +0700

    feat: update the profile section in courier pov

--- src/screen/courier/courier-payment.tsx
+++ src/screen/courier/courier-payment.tsx
@@ -1,3 +1,4 @@
+import { Hourglass, Wallet } from "lucide-react";
 import MainLayout from "../../component/main-layout";
 
 export default function CourierPayment() {
@@ -5,8 +6,52 @@ export default function CourierPayment() {
     <MainLayout needProtection={true} isCourier={true}>
       <div className="w-full pb-32">
         <div className="px-5 py-5 flex flex-col gap-4">
-          <h1 className="text-xl pb-8" >Payment</h1>
-          CourierPayment
+          <h1 className="text-xl">Pending Payment</h1>
+          <div className="flex justify-between items-center p-5 rounded-md border border-gray-300">
+            <div className="flex items-center gap-3">
+              <Hourglass />
+              <p className="text-sm">Tempor tempor esse nisi.</p>
+            </div>
+            <p className="text-sm text-gray-600">10/25</p>
+          </div>
+          <div className="flex justify-between items-center p-5 rounded-md border border-gray-300">
+            <div className="flex items-center gap-3">
+              <Hourglass />
+              <p className="text-sm">Tempor tempor esse nisi.</p>
+            </div>
+            <p className="text-sm text-gray-600">20/25</p>
+          </div>
+          <div className="flex justify-between items-center p-5 rounded-md border border-gray-300">
+            <div className="flex items-center gap-3">
+              <Hourglass />
+              <p className="text-sm">Tempor tempor esse nisi.</p>
+            </div>
+            <p className="text-sm text-gray-600">10/25</p>
+          </div>
+        </div>
+        <div className="px-5 py-5 flex flex-col gap-4">
+          <h1 className="text-xl">Claim Payment</h1>
+          <div className="flex justify-between items-center p-5 rounded-md border border-gray-300">
+            <div className="flex items-center gap-3">
+              <Wallet />
+              <p className="text-sm">Tempor tempor esse nisi.</p>
+            </div>
+            <p className="text-sm text-gray-600">claim</p>
+          </div>
+          <div className="flex justify-between items-center p-5 rounded-md border border-gray-300">
+            <div className="flex items-center gap-3">
+              <Wallet />
+              <p className="text-sm">Tempor tempor esse nisi.</p>
+            </div>
+            <p className="text-sm text-gray-600">caim</p>
+          </div>
+          <div className="flex justify-between items-center p-5 rounded-md border border-gray-300">
+            <div className="flex items-center gap-3">
+              <Wallet />
+              <p className="text-sm">Tempor tempor esse nisi.</p>
+            </div>
+            <p className="text-sm text-gray-600">claim</p>
+          </div>
         </div>
       </div>
     </MainLayout>
--- src/screen/courier/courier-profile.tsx
+++ src/screen/courier/courier-profile.tsx
@@ -1,10 +1,21 @@
+import { UserCircle } from "lucide-react";
 import MainLayout from "../../component/main-layout";
 
 export default function CourierProfile() {
   return (
     <MainLayout needProtection={true} isCourier={true}>
-      <div>
-        CourierProfile
+      <div className="w-full pb-32">
+        <div className="p-5">
+          <div className="w-full flex items-center gap-5 bg-blue-500 p-5 rounded-md">
+            <div>
+              <UserCircle className="w-10 h-10 text-white" />
+            </div>
+            <div className="text-white">
+              <p>NAMA LENGKAP DISINI</p>
+              <p>Courier/Customer</p>
+            </div>
+          </div>
+        </div>
       </div>
     </MainLayout>
   );
